24c67c3b5b7c3f94e18ce4096502d79278aea55b
marcpublic <129193220+marcpublic@users.noreply.github.com>
2023-03-31 09:38:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
-import os
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from logic import Add, Subtract, Multiply, Divide
+
 
 app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Add, "/add")
+api.add_resource(Subtract, "/subtract")
+api.add_resource(Multiply, "/multiply")
+api.add_resource(Divide, "/division")
 
 
-@app.route("/")
-def hello():
-    return "Hello Thales!!!"
+@app.route('/')
+def hello_world():
+    return "Hello World!"
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
