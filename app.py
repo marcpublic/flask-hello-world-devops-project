cfbf3b1019f6a905a23e523c32173c2883d462e6
root <root@ip-172-31-6-115.eu-west-3.compute.internal>
2023-03-31 09:11:49 +0000

    new

--- app.py
+++ app.py
@@ -1,23 +1,86 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-from logic import Add, Subtract, Multiply, Divide
-import os
+#!/bin/env python3
 
+import flask
+from elasticapm.contrib.flask import ElasticAPM
 
-app = Flask(__name__)
-api = Api(app)
+TPL = flask.render_template # Pour éviter de toujours taper flask.render_template...
 
-api.add_resource(Add, "/add")
-api.add_resource(Subtract, "/subtract")
-api.add_resource(Multiply, "/multiply")
-api.add_resource(Divide, "/division")
+app = flask.Flask(__name__, template_folder='.')
+app.config['ELASTIC_APM'] = {
+  'SERVICE_NAME': 'flask-service',
+  'SECRET_TOKEN': 'LG5PoPO8Mw9fJ9Dq1d',
+  'SERVER_URL': 'https://822e8d5da0df47de93ba7d00e69b6c38.apm.eu-west-1.aws.cloud.es.io:443',
+  'ENVIRONMENT': 'my-deployment',
+}
 
+apm = ElasticAPM(app)
 
 @app.route('/')
-def hello_world():
-    return "Hello Thales!!!"
+def info():
+    data = """\
+    Bonjour Visiteur
+    """
+    return TPL("default.html", title='Home', data=data)
+
+@app.route('/paramurl/<int:number>')
+def paramurl(number):
+    data = """\
+    Vous avez mis  {} dans l'URL.
+    """.format(number)
+    return TPL("default.html", title='ParamUrl', data=data)
+
+@app.route('/paramget')
+def paramget():
+    login = flask.request.args['login']
+    data = """\
+    Le login entré est {}.
+    """.format(login)
+    return TPL("default.html", title='Paramget', data=data)
+
+@app.route('/formulaire')
+def formulaire():
+    data = """\
+    <form action="validate" method="post">
+    <input type="text" name="login"/><br/>
+    <input type="submit"/>
+    </form>
+    """
+    return TPL("default.html", title='Formulaire', data=data)
+
+# Récupération des données d'un formulaire, en POST uniquement
+# Si les données sont postées en json et non en
+# application/x-www-form-urlencoded ou multipart/form-data
+# utiliser flask.request.get_json()  au lieu
+# de flask.request.form
+@app.route('/validate', methods=["POST"])
+def validate():
+    login = flask.request.form['login']
+    data = """\
+    Le login entré est {}.
+    """.format(login)
+    return TPL("default.html", title='Validate', data=data)
 
 
-if __name__=="__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# Renvoie du json
+@app.route('/data_json')
+def data_json():
+    liste = [1, 2, 3, "toto"]
+    dico = {"val1": liste, "val2": "Salut"}
+    return flask.jsonify(dico) # Renvoie la chaîne json en gérant correctement l'entête HTTP
+
+# Code de retour :
+# https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP
+@app.route('/forbidden')
+def forbidden():
+    flask.abort(403)
+
+@app.route('/redirect_me')
+def redirect_me():
+    return flask.redirect(flask.url_for('info'))
+
+print("PATH =====>", app.instance_path)
+if __name__ == '__main__':
+    app.config['DEBUG'] = False
+    app.secret_key = 'chooseaverysecretkeyhere'
+#    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
